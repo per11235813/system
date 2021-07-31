9688d226c06c1b370d24c268909642e58d38c01a
Per Jensen <per11235813@gmail.com>
2021-07-31 10:05:04 +0200

    backup

--- wsl/setup-user.sh
+++ wsl/setup-user.sh
@@ -2,7 +2,7 @@ python3 -m venv ~/venv
 . ~/venv/bin/activate
 
 python -m pip install -U pip setuptools wheel
-pip install jupyter invoke pydantic pandas black
+pip install jupyter invoke pydantic pandas black flake8 fastapi
 
 echo ". ~/venv/bin/activate"  >> ~/.bashrc
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
