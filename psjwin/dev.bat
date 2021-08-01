9feb277a3e95e07f140e0cb47658c3e942156caf
Per Stoffer Jensen <per.jensen.a@man-es.com>
2021-08-01 10:59:45 +0200

    xx

--- psjwin/dev.bat
+++ psjwin/dev.bat
@@ -1,2 +1,10 @@
+
+set pythonpath=%userprofile%\repo\python;%cd%\src;%cd%\tests;%cd%
+set SHELL=cmd.exe
+
+doskey ls=dir /B
+doskey ll=dir
+doskey jup=jupyter lab --ContentsManager.allow_hidden=True
+
 call %userprofile%\venv\scripts\activate
 
---
+++ psjwin/pinv.bat
@@ -0,0 +1,3 @@
+rem @echo off
+
+%userprofile%\venv\Scripts\inv.exe -r %userprofile%\repo\python -c tasks %*
